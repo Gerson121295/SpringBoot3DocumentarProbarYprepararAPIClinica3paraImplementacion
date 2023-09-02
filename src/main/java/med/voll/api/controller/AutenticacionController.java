c44e68b1371a93cbf77a9eb6f0c8da8229601142
Gerson121295 <Gerson21millones@gmail.com>
2023-09-02 11:10:35 -0600

    05.2-Update nombre de tablas BD.

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -1,7 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
+import med.voll.api.domain.usuarios.DatoAutenticarUsuario;
 import med.voll.api.domain.usuarios.Usuario;
 import med.voll.api.infra.security.DatosJwtToken;
 import med.voll.api.infra.security.TokenService;
@@ -28,10 +28,10 @@ public class AutenticacionController {
 
     //Probar el metodo POST: http://localhost:8080/login  enviar:    {  "login":"gerson.ep","clave":"123456" }
     @PostMapping
-    public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){ //Necesitamos un DTO porque vamos a recibir parametros del cliente.
+    public ResponseEntity autenticarUsuario(@RequestBody @Valid DatoAutenticarUsuario datoAutenticarUsuario){ //Necesitamos un DTO porque vamos a recibir parametros del cliente.
 
-        Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(),
-                datosAutenticacionUsuario.clave()); //Genero con los datosAutenticacionUsuario el token
+        Authentication authToken = new UsernamePasswordAuthenticationToken(datoAutenticarUsuario.login(),
+                datoAutenticarUsuario.clave()); //Genero con los datosAutenticacionUsuario el token
         var usuarioAutenticado = authenticationManager.authenticate(authToken); //El token es usado por el authenticationManger para autenticar. El usuarioAutenticado va a salir si esta autenticacion es exitosa.
         var JWTtoken  = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal()); //El getPrincipal es el usuario o Objeto que ya fue autenticado en nuestro sistema.
         return ResponseEntity.ok(new DatosJwtToken(JWTtoken)); //Devolvera un DTO con los datos user, id y fecha de expiracion
---
+++ src/main/java/med/voll/api/domain/usuarios/DatoAutenticarUsuario.java
@@ -0,0 +1,6 @@
+package med.voll.api.domain.usuarios;
+
+
+public record DatoAutenticarUsuario(String login, String clave) { //Clase DTO para recibir datos del cliente
+
+}
